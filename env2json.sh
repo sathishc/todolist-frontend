34a5285dc96ae4205a8388fa7e891b6e823ce91b
Sathish <sat.hariharan@gmail.com>
2021-12-08 23:01:38 +0530

    update the apiname key

--- env2json.sh
+++ env2json.sh
@@ -1,3 +1,3 @@
 cat << EOF > src/cdk-exports.json
-{"BackendStack":{"apiName": "$TODOLIST_API_NAME", "awscognitoregion": "$TODO_REGION", "userpool": "$USERPOOL_ID", "webclientid": "$WEB_CLIENT_ID", "identitypool": "$IDENTITYPOOL_ID", "apiendpoint": "$TODOLIST_API_URL"}}
+{"BackendStack":{"apiname": "$TODOLIST_API_NAME", "awscognitoregion": "$TODO_REGION", "userpool": "$USERPOOL_ID", "webclientid": "$WEB_CLIENT_ID", "identitypool": "$IDENTITYPOOL_ID", "apiendpoint": "$TODOLIST_API_URL"}}
 EOF
